527520d6f4bee527ae658b4acc5fe449fca685d0
MissSindani <133926933+MissSindani@users.noreply.github.com>
2023-09-23 16:32:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const firstEmail = document.querySelector("#firstEmail").value ;
+const lastEmail = document.querySelector("#secondEmail").value ;
+const firstPassword = document.querySelector("#firstPassword").value ;
+const lastPassword = document.querySelector("#confirmPassword"). value ;
+
+if (firstEmail !== lastEmail ){
+    console("Emails Do Not Match")
+} else{
+    console("Email Address Match")
+};
+
+
+  
